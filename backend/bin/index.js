8d517139abc09f4582f4b2d0cc7c16f8dbb8295d
Abyan Rizqo <rizqo1412@gmail.com>
2021-10-16 07:08:25 +0700

    Fix CLI app flow

--- backend/bin/index.js
+++ backend/bin/index.js
@@ -7,11 +7,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const Session = require('../models/mongoose/session.js');
 const program = new Command();
 
-mongoose.connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+
 
 program
 .name("refactory-assignment")
@@ -21,18 +17,28 @@ program
 .command('get-login-users')
 .description("Show all users that currently login")
 .alias('glu')
-.action(() => {
-    Session.find({}).then((currentUser) => {
-        if (currentUser.length > 0) {
-            console.log("List of login users:");
-            currentUser.forEach((item, index)=>{
-                console.log('- ',item.session.email);
-            })
-            console.log('------------------------');
-        } else {
-            console.log("Currently there is no user that login");
-        }
-    });
+.action(async () => {
+    try {
+        mongoose.connect(MONGODB_URI, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        Session.find({}).then((currentUser) => {
+            if (currentUser.length > 0) {
+                console.log("List of login users:");
+                currentUser.forEach((item, index)=>{
+                    console.log('- ',item.session.email);
+                })
+                console.log('------------------------');
+            } else {
+                console.log("Currently there is no user login");
+            }
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
+    
 });
 
 program.parse(process.argv);
